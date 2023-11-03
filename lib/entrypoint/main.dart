70531996499a92849cee17c41d04cfcc86bb4c0e
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2023-11-03 22:08:34 +0400

    Add DI support using Get It library

--- lib/entrypoint/main.dart
+++ lib/entrypoint/main.dart
@@ -1,6 +1,8 @@
+import 'package:bab_alomda_assessment_flutter/di/di_container.dart';
 import 'package:bab_alomda_assessment_flutter/presentation/app/app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await initDiContainer();
   runApp(const MyApp());
 }
