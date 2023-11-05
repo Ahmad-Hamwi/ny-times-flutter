a000cff750cc513ba1c0da727b43b9ebb55edb8c
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2023-11-05 19:35:08 +0400

    Implements article details page

--- lib/presentation/pages/article_details/article_details_page.dart
+++ lib/presentation/pages/article_details/article_details_page.dart
@@ -1,4 +1,5 @@
 import 'package:bab_alomda_assessment_flutter/domain/entity/article_entity.dart';
+import 'package:bab_alomda_assessment_flutter/presentation/widgets/remote_image.dart';
 import 'package:flutter/material.dart';
 
 class ArticleDetailsPageArgs {
@@ -14,15 +15,65 @@ class ArticleDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final title = args.article.section.name[0].toUpperCase() +
+        args.article.section.name.substring(1);
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        titleSpacing: 0,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'ArticleDetails Page',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            RemoteImage(src: args.article.largeImageSrc),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    args.article.title,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    args.article.author,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    args.article.description,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    "Read more...",
+                    style: TextStyle(
+                      color: Colors.blue[800],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
